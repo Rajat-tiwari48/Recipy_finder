4891f976e08a4efa810734c23cacd3e0d6a481cc
Rajat <115857854+Rajat-tiwari48@users.noreply.github.com>
2025-10-30 02:21:36 +0530

    Clean up Loader component by removing comments
    Removed commented-out code for Loader component.

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,26 +1,3 @@
-// import React from "react";
-
-// // export default function Loader({ size = 6 }) {
-// //   return (
-// //     <div className={`flex items-center justify-center py-6`}>
-// //       <div className={`w-${size} h-${size} rounded-full animate-spin border-4 border-t-transparent`} />
-// //     </div>
-// //   );
-// // }
-
-// function Loader() {
-//   return (
-//     <div className="flex items-center justify-center py-12">
-//       <div className="relative">
-//         <div className="w-16 h-16 border-4 border-orange-200 rounded-full animate-spin border-t-orange-500" />
-//         <ChefHat className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-orange-500" size={24} />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Loader;
-
 import React from "react";
 import { ChefHat } from "lucide-react";
 
